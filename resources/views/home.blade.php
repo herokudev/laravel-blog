0e91ce5caac6c8872099cf53345b3df179455ef3
Herbert Orellana <ao43085@gmail.com>
2023-08-31 18:11:01 -0600

    Adding posts

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,9 +9,29 @@
     @auth
         <p>Congrats you are logged in.</p>
         <form action="/logout" method="post">
+            @csrf
+            <button>Log out</button>
+        </form>
+        <p></p>
+        <div style="border: 3px solid black;">
+        <h2>Create a new post</h2>
+            <form action="/create-post" method="post">
                 @csrf
-                <button>Log out</button>
+                <input type="text" name="title" placeholder="enter post title">
+                <textarea name="body" placeholder="enter post body"></textarea>
+                <button>Save Post</button>
             </form>        
+        </div>
+        <p></p>
+        <div style="border: 3px solid black;">
+            <h2>All posts</h2>
+            @foreach ($posts as $post)
+                <div style="background-color: gray; padding: 10px; margin: 10px;">
+                    <h3>{{$post['title']}}</h3>
+                    {{$post['body']}}
+                </div>
+            @endforeach
+        </div>
     @else
         <div style="border: 3px solid black;">
             <h2>Register</h2>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
 
 /*
@@ -15,9 +17,12 @@ use App\Http\Controllers\UserController;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $post = Post::where('id_user', auth()->id())->latest()->get();
+    return view('home', ['posts' => $post]);
 });
 
 Route::post('/register', [UserController::class, 'register']);
 Route::post('/logout', [UserController::class, 'logout']);
 Route::post('/login', [UserController::class, 'login']);
+
+Route::post('/create-post', [PostController::class, 'createPost']);
